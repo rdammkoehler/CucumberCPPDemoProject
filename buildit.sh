e63e1346d545fbf4631d29527423385c0d109dc5
Christopher Freeman <cwfreeman@gmail.com>
2012-09-05 23:31:06 -0500

    Fixed absolute library path.

--- buildit.sh
+++ buildit.sh
@@ -1,4 +1,4 @@
 rm -f *.o TestStringReverser
 g++ $(gtest-config --cppflags --cxxflags) -o TestStringReverser.o -c TestStringReverser.cpp \
-&& g++ $(gtest-config --ldflags --libs) -o TestStringReverser TestStringReverser.o /usr/lib/libgtest_main.a \
+&& g++ $(gtest-config --ldflags --libs) -o TestStringReverser TestStringReverser.o $(gtest-config --libdir)/libgtest_main.a \
 && ./TestStringReverser
